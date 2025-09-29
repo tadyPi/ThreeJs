99a99e1b78aa98994775f7d6b536e5088d50aafd
TadyPi <Hldyson@hotmail.com>
2025-09-29 19:32:15 +0000

    Update page title to 'Load of Pixels | Three.JS' and adjust text color in texture drawing for better visibility

--- threejs.js
+++ threejs.js
@@ -25,7 +25,7 @@ function init() {
     context.drawImage(texture.image, 0, 0);
 
     // Draw the text onto the canvas
-    context.fillStyle = "rgba(169, 169, 169, 1)";
+    context.fillStyle = "rgba(185, 185, 185, 1)";
     context.font = "8rem Arial";
     context.fillText("loadofpixels.com", 30, 528);
 
